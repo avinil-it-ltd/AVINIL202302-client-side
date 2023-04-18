af3e154b4cf4c8fcec790938007d115cd8497de2
Avishekdevnath <avishekdevnath@gmail.com>
2023-04-18 12:54:55 +0600

    A

--- src/Component/Authentication/SignUp.js
+++ src/Component/Authentication/SignUp.js
@@ -5,34 +5,27 @@ import { Link } from 'react-router-dom';
 import { AuthContext } from '../../AuthProvider/AuthProvider';
 
 const SignUp = () => {
-    const {createUser} = useContext(AuthContext)
-    const handleRegister = e =>{
+    const { createUser } = useContext(AuthContext)
+    const handleRegister = e => {
 
-    e.preventDefault();
-    console.log(e);   
-    const form = e.target;
+        e.preventDefault();
+        console.log(e);
+        const form = e.target;
         const name = e.target[0].value;
         const email = form[1].value;
-        createUser(email,123456)
-        .then(result=>{
+        createUser(email, 123456)
+            .then(result => {
 
-            const user=result.user;
-            //navigate(from,{replace: true})
-
-            
-
-
-        })
-        .catch(error=>console.error(error))
-       // const password = form.password.value;
-    console.log(name,email);
-   }
+                const user = result.user;
+                //navigate(from,{replace: true})
+            })
+            .catch(error => console.error(error))
+        // const password = form.password.value;
+        console.log(name, email);
+    }
     return (
         <div className='bg-slate-900 '>
             <Navbar></Navbar>
-
-
-
             <section class="text-white body-font relative">
                 <div class="container px-5 py-24 mx-auto">
                     <div class="flex flex-col text-center w-full ">
@@ -41,20 +34,20 @@ const SignUp = () => {
 
                     <div class="lg:w-1/2 md:w-2/3 mx-auto bg-slate-800  p-16 rounded-sm">
                         {/* form code start here */}
-                        <form onSubmit={handleRegister}  class=" flex w-full flex-col md:flex-row lg:flex-row flex-wrap -m-2">
+                        <form onSubmit={handleRegister} class=" flex w-full flex-col md:flex-row lg:flex-row flex-wrap -m-2">
 
                             {/* name field start here */}
                             <div class="p-2 w-full md:w-1/2  lg:w-72 ">
                                 <div class="relative">
                                     <label for="name" class="leading-7 text-sm ">Name</label>
-                                    <input type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700  px-3 leading-8 transition-colors duration-200 ease-in-out " />
+                                    <input type="text" id="name" name="name_teach" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700  px-3 leading-8 transition-colors duration-200 ease-in-out " />
                                 </div>
                             </div>
                             {/* Email field start here */}
                             <div class="p-2 w-full md:w-1/2  lg:w-72 ">
                                 <div class="relative">
                                     <label for="email" class="leading-7 text-sm ">Email</label>
-                                    <input type="email" id="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700  px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                    <input type="email" id="email" name="email_teach" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700  px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                 </div>
                             </div>
 
@@ -67,19 +60,13 @@ const SignUp = () => {
                                 <input id="student" class="mr-2 peer/student cursor-pointer" type="radio" name="status" />
                                 <label for="student" class="cursor-pointer peer-checked/student:text-sky-500">student</label>
 
-
-
-
-
-
-
                                 {/* teacher block */}
                                 <div class="hidden peer-checked/teacher:block w-full">
                                     <div className='flex w-full flex-col md:flex-row lg:flex-row flex-wrap'>
                                         {/* Group field start here */}
                                         <div class="py-2 pr-2 w-full md:w-1/2  lg:w-72 ">
                                             <div class="relative">
-                                                <label for="email" class="leading-7 text-sm ">Expertise Group</label>
+                                                <label for="group_teach" class="leading-7 text-sm ">Expertise Group</label>
                                                 <select class="select w-full select-sm bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none  text-gray-700  px-3  py-1 transition-colors duration-200 ease-in-out">
                                                     <option disabled selected>Pick your Group</option>
                                                     <option className=' '>Commerce</option>
@@ -93,13 +80,13 @@ const SignUp = () => {
                                         <div class="py-2 pr-2 w-full md:w-1/2  lg:w-72 ">
                                             <div class="relative">
                                                 <label for="name" class="leading-7 text-sm ">Address</label>
-                                                <input type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700  px-3 leading-8 transition-colors duration-200 ease-in-out " />
+                                                <input type="text" id="name" name="address_teach" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700  px-3 leading-8 transition-colors duration-200 ease-in-out " />
                                             </div>
                                         </div>
 
                                         <div class=" w-full ">
                                             <div class="relative">
-                                                <label for="email" class="leading-7 text-sm ">Upload Your Image</label>
+                                                <label for="image_teach" class="leading-7 text-sm ">Upload Your Image</label>
                                                 <input type="file" class="file-input file-input-bordered file-input-sm w-full max-w-xs bg-gray-100 bg-opacity-50 rounded border border-gray-300" />
                                             </div>
                                         </div>
@@ -108,15 +95,6 @@ const SignUp = () => {
 
                                 </div>
 
-
-
-
-
-
-
-
-
-
                                 {/* student block */}
                                 <div class="hidden peer-checked/student:block ">
                                     <div className='flex w-full flex-col md:flex-row lg:flex-row flex-wrap '>
@@ -124,7 +102,7 @@ const SignUp = () => {
                                         <div class="p-2 w-full md:w-1/2  lg:w-72">
                                             <div class="relative">
                                                 <label for="email" class="leading-7 text-sm ">Roll</label>
-                                                <input type="Roll" id="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700  px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                                <input type="Roll" id="email" name="roll_stu" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700  px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                             </div>
                                         </div>
                                         {/* Group field start here */}
@@ -144,14 +122,14 @@ const SignUp = () => {
                                         <div class="p-2 w-full md:w-1/2  lg:w-72">
                                             <div class="relative">
                                                 <label for="email" class="leading-7 text-sm ">Mobile</label>
-                                                <input type="email" id="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700  px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                                <input type="mobile_stu" id="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700  px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                             </div>
                                         </div>
 
                                         {/* Session field start here */}
                                         <div class="p-2 w-full md:w-1/2  lg:w-72">
                                             <div class="relative">
-                                                <label for="email" class="leading-7 text-sm ">Session</label>
+                                                <label for="session_stu" class="leading-7 text-sm ">Session</label>
                                                 <select class="select w-full select-sm bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none  text-gray-700  px-3  py-1 transition-colors duration-200 ease-in-out">
                                                     <option disabled selected>Pick your session year</option>
                                                     <option>2023</option>
@@ -169,12 +147,12 @@ const SignUp = () => {
 
                                         <div class="p-2 w-full ">
                                             <div class="relative">
-                                                <label for="email" class="leading-7 text-sm ">Upload Your Image</label>
+                                                <label for="image" class="leading-7 text-sm ">Upload Your Image</label>
                                                 <input type="file" class="file-input file-input-bordered file-input-sm w-full max-w-xs bg-gray-100 bg-opacity-50 rounded border border-gray-300" />
                                             </div>
                                         </div>
 
-                                        
+
                                     </div>
                                 </div>
                             </fieldset>
