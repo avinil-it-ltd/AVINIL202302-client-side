5af2f6518754eabdb2d9d0c901e88addf361c918
Avishekdevnath <avishekdevnath@gmail.com>
2023-04-19 15:07:33 +0600

    A

--- src/Component/Authentication/SignUp.js
+++ src/Component/Authentication/SignUp.js
@@ -5,6 +5,8 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../../AuthProvider/AuthProvider";
 import Select from "react-select";
 import { useForm } from "react-hook-form";
+import axios from "axios";
+import SweetAlert from "react-swal";
 
 const SignUp = () => {
   const { register, handleSubmit } = useForm();
@@ -12,10 +14,10 @@ const SignUp = () => {
 
   const { createUser } = useContext(AuthContext);
   const [type, setType] = useState("null")
-  const handleType = e=>{
+  const handleType = e => {
 
-    const roll_type =e.target.value; 
-    
+    const roll_type = e.target.value;
+
     setType(roll_type);
     console.log(type);
   }
@@ -24,42 +26,106 @@ const SignUp = () => {
     // const [name,email, group, image, roll, session, address] = e[0];
     console.log(e);
     // console.log(name);
-    
+
     const userObject = {
-      "name": e.name,
       "email": e.email,
+      "userName": e.name,
+      "userPass": e.password,
+      "userType": e.type,  //student or teacher
+      "imageUrl": e.image,
+      "roll": e.roll || 0,
       "group": e.group,
-      "image": e.image,
-      "roll": e.roll,
-      "address": e.address,
-      "type": e.type,
       "session": e.session,
       "phone": e.phone,
-      "password": e.password
-    }
-    console.log(userObject); 
-    fetch('http://localhost:5000/api/v1/create-user', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(userObject)
-    })
-    // {console.log(userObject); }
-
-      .then(res => res.json())
-      .then(data => {
-        if (data.insertedId) {
-          console.log(userObject); 
-          console.log("form submitted successfully")
-        }
-        else{
-          console.log("Error")
-        }
-      })
-
-
+      "address": e.address,
+      "subjectName": "null",             
+      "fatherName":"null",
+      "motherName": "null", 
+      "qualification": e.qualification,
       
+
+    }
+
+
+      //   {
+      //     "email":"bijon1@gmail.com",
+      //         "userName":"bijon",
+      //         "userPass":"bijojoj",
+      //         "userType":"student", //student or teacher
+      //         "imageUrl":"url",
+
+      //         "address":"chandanaish",
+      //         "fatherName":"bijon",
+      //         "motherName":"bijon",
+      //         "subjectName":"bangla",
+      //         "qualification":"developer"
+
+      // }
+    // }
+    // const userObject = {
+    //   email: "avishadek@gmail.com",
+      // userName: "bijon",
+      // userPass: "bijojoj",
+      // userType: "student", //student or teacher
+      // imageUrl: "url",
+
+      // address: "chandanaish",
+      // fatherName: "bijon",
+      // motherName: "bijon",
+      // subjectName: "bangla",
+      // qualification: "developer",
+      
+    // };
+    console.log(userObject);
+
+    axios({
+      method: "post",
+      url: "http://localhost:5000/api/v1/create-user",
+      userObject,
+    })
+      // axios.post("/api/wishlists", { 
+      //   product_title: product_title,
+      //   product_price: product_price,
+      //   user_rating: user_rating,
+      //   product_stock: product_stock,
+      //   product_imageUrl: product_imageUrl.thumbnail,
+
+      //  })
+      .then((response) => {
+
+        if (response.data.insertedId) {
+          // setControl(!control);
+          // SweetAlert("WOW!!!  product add successfully");
+          console.log(response.data.insertedId,true)
+        } else {
+          console.log(false)
+          // setControl(false);
+          // SweetAlert("Failed");
+        }
+      });
+    // console.log(userObject); 
+    // fetch('http://localhost:5000/api/v1/create-user', {
+    //   method: 'POST',
+    //   headers: {
+    //     'content-type': 'application/json'
+    //   },
+    //   body: JSON.stringify(userObject)
+    // })
+    // // {console.log(userObject); }
+
+    //   .then(res => res.json())
+    //   .then(data => {
+    //     if (data.insertedId) {
+    //       console.log(userObject); 
+    //       console.log("form submitted successfully")
+    //     }
+    //     else{
+    //       console.log("Error")
+    //     }
+    //   })
+
+
+
     // console.log(e);
     // const form = e.target;
     // const name = form.name.value;
@@ -208,13 +274,13 @@ const SignUp = () => {
                 </div>
               </div>
               <select {...register("type")} class="select w-full select-sm bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none  text-gray-700  px-3  py-1 transition-colors duration-200 ease-in-out">
-                  <option disabled selected>
-                    Select the position
-                  </option>
-                  <option className="" value="teacher">Teacher</option>
-                  <option className="" value="student">Student</option>
-                  
-                </select>
+                <option disabled selected>
+                  Select the position
+                </option>
+                <option className="" value="teacher">Teacher</option>
+                <option className="" value="student">Student</option>
+
+              </select>
               <fieldset className="px-2 my-4">
                 <span className="mr-10 md:mr-0 md:pr-10 lg:pr-10">
                   Select the position
@@ -227,7 +293,7 @@ const SignUp = () => {
                   class="mr-2 peer/teacher cursor-pointer"
                   type="radio"
                   name="status"
-                  // onChange={handleType}
+                // onChange={handleType}
 
                 />
 
@@ -237,7 +303,7 @@ const SignUp = () => {
                   class="mr-2 md:mr-5 lg:mr-16 cursor-pointer peer-checked/teacher:text-sky-500"
                 >
                   teacher
-                </label> 
+                </label>
 
                 <input
                   // {...register({"student":true})}
@@ -278,21 +344,21 @@ const SignUp = () => {
                     </div>
 
                     {/* name field start here */}
-                    {/* <div class="py-2 pr-2 w-full md:w-1/2  lg:w-72 ">
+                    <div class="py-2 pr-2 w-full md:w-1/2  lg:w-72 ">
                       <div class="relative">
                         <label for="address" class="leading-7 text-sm ">
-                          Address
+                          Qualification
                         </label>
                         <input
-                  {...register("address_t")}
+                          {...register("qualification")}
 
                           type="text"
-                          id="address"
-                          name="address"
+                          id="qualification"
+                          name="qualification"
                           class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700  px-3 leading-8 transition-colors duration-200 ease-in-out "
                         />
                       </div>
-                    </div> */}
+                    </div>
 
                     {/* <div class=" w-full ">
                       <div class="relative">
